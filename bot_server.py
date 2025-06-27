5190249f2e715e7f8a3bde7d8bc9b7bb7b732eb1
dominikoricardo <46958526+dominikoricardo@users.noreply.github.com>
2025-06-27 21:14:54 +0500

    Add files via upload

---
+++ bot_server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import requests
+import os
+
+app = Flask(__name__)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+@app.route("/sms", methods=["POST"])
+def sms_handler():
+    data = request.get_json()
+    message = data.get("message") or "Пустое сообщение"
+
+    text = f"📩 СМС от банка:\n{message}"
+    requests.get(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        params={"chat_id": CHAT_ID, "text": text}
+    )
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
